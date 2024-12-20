ff5e36381b403a0259706dce575975dd6409154f
Camille Steiner <camille.steiner@epfl.ch>
2024-12-20 14:02:22 +0100

    je vais tenter un truc jspr ca casse pas mon graph

--- src/scripts/Part3/actor_award_visualization.py
+++ src/scripts/Part3/actor_award_visualization.py
@@ -7,19 +7,44 @@ def create_actor_award_visualization():
     oscars = pd.read_csv("src/data/oscar_clean.csv", quotechar='"')
     data = data.head(nb_acteurs)
 
+    # Create a sunburst chart with Plotly
+    color_mapping = {
+        "(?)":"#AED6F1"  ,
+        "Title": "#FFC1CC",  # Rose poudré
+        "Acting": "#A4DDED",  # Bleu ciel pastel
+        "Directing": "#D6BBFC",  # Lavande légère
+        "Writing": "#FFDAB9",  # Pêche pâle
+        "Music": "#FFFACD",  # Jaune crème doux
+        "Production": "#C7F9CC",  # Menthe fraîche
+        "SciTech": "#BFE3C4",  # Vert sauge pastel
+        "Special": "#EEDCFB"  # Mauve délicat
+    }
+
+
     data = data.merge(oscars, how='inner', on='freebase_actor_id')
     data = data.dropna(subset=['CanonicalCategory'])
     data = data.dropna(subset=['Class'])
     data = data.dropna(subset=['Actor'])
+    data = data.assign(Count=1)
+    data = data.groupby(['Actor', 'Class', 'CanonicalCategory']).sum().reset_index()
 
-    # Create a sunburst chart with Plotly
-    print(data)
+
+
+
     fig = px.sunburst(
         data,
         path=['Actor', 'Class', 'CanonicalCategory'],  # Define the hierarchy
         values='score',  # Size of each segment
         color='Class',  # Use category to determine color,
+        color_discrete_map= color_mapping,
         title='Actors and Their Awards by Category and Subcategory',
+        hover_data={"Actor": True,
+                "CanonicalCategory":True,
+                "Class":True,
+                "Winner":True,
+                "Count":True,
+                "score": False},
+        template="plotly_white",
         width=1800, height=1600
     )
 
